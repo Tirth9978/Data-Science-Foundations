747de20a37ac642b081d3a500d684df618811cfb
Tirth9978 <tirthppatel9978@gmail.com>
2025-08-25 21:20:19 +0530

    I have done with inexing

---
+++ Numpy/08 ) Indexing in Array.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Numpy uses the Zero-based indexing ......
+# There is the Negative indexing in the Numpy array 
+
+
+"""
+for 1D Array -> arr[index]
+for 2D Array -> arr[row , column]
+"""
+
+arr = np.array([1,2,3,4,5])
+print(arr[0])
+print(arr[1])
+print(arr[2])
+print(arr[3])
+print(arr[4])
